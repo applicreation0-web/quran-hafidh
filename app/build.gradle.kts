ef3458755a632c813d44204c269d22a9e82e0480
applicreation0-web <applicreation0@gmail.com>
2026-08-31 16:09:38 +0100

    Bump Quran Safeguard to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.applicreation0.quransafeguard"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.3.0"
+        versionCode = 3
+        versionName = "0.3.1"
     }
 
     buildFeatures {
