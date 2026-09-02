349ebe4f725d99e7a043d8bb7640a913596ef0a0
applicreation0-web <applicreation0@gmail.com>
2026-09-02 19:30:27 +0100

    Complete restricted-settings help card import

--- app/src/main/java/com/quranunlock/guard/ProtectionSetupActivity.kt
+++ app/src/main/java/com/quranunlock/guard/ProtectionSetupActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateOf
